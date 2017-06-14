b0ed1f7e8391c175e1e14f3415dc9abbf0278997
momortral <matthieu.taieb@orange.fr>
2017-06-14 10:54:17 +0200

    acceuil

--- Vue/accueil_user.php
+++ Vue/accueil_user.php
@@ -21,10 +21,31 @@ require ("Header.php");
 
 <?php
 //$id = $_SESSION['id'];
+$_SESSION['prenom'] = 'matthieu';
 ?>
 
 <!-- A remplacer par l'acceuil -->
 <div class="center">
+    <div class="bonjour">
+        <p>Bonjour <?php echo $_SESSION['prenom'] ?>,</p>
+        <p>Ravi de vous revoir !</p>
+    </div>
+    <div class="pieceacceuil">
+        <?php
+        $req = $bdd->query('SELECT id, nom_piece FROM piece WHERE id_maison = 1');
+        while ($pieces = $req->fetch()){ ?>
+            <div>
+                <h2><?php echo $pieces['nom_piece']?></h2>
+                <?php
+                $req2 = getCapteurSalle($bdd, $pieces['id']);
+                while ($donnees = $req2->fetch()){ ?>
+                    <ul>
+                        <li><?php echo $donnees['nom_capteur'] ?> : <?php echo $donnees['donnee'] ?></li>
+                    </ul>
+                <?php } ?>
+            </div>
+        <?php } ?>
+    </div>
     <div class="derniermsg">
         <h1>Vos derniers messages :</h1>
         <?php $derniers_messages =  getListeMessageUser($bdd);
@@ -33,23 +54,6 @@ require ("Header.php");
         <?php }
         ?>
         <a href="contact.php">Vous voulez renvoyez un mail?</a>
-    </div>
-    <div class="pieceacceuil">
-    <?php
-    $req = $bdd->query('SELECT id, nom_piece FROM piece WHERE id_maison = 1');
-    while ($pieces = $req->fetch()){ ?>
-        <div>
-            <h2><?php echo $pieces['nom_piece']?></h2>
-            <?php
-            $req2 = getCapteurSalle($bdd, $pieces['id']);
-            while ($donnees = $req2->fetch()){ ?>
-                <ul>
-                    <li><?php echo $donnees['nom_capteur'] ?> : <?php echo $donnees['donnee'] ?></li>
-                </ul>
-            <?php } ?>
-        </div>
-    <?php } ?>
-    </div>
     </div>
 </div>
 
