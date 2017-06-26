2f5373f68ddbba2586f1fa3e20c2bcea2e9345eb
momortral <matthieu.taieb@orange.fr>
2017-06-26 13:34:16 +0200

    INTEGRATION TRAME

--- Modele/capteur-db.php
+++ Modele/capteur-db.php
@@ -76,5 +76,56 @@ function getCapteurSalle($bdd, $piece){
     }
 }
 
+function donneesTrame(){
+    $ch = curl_init();
+    curl_setopt(
+        $ch,
+        CURLOPT_URL,
+        "http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=9999");
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    echo "Raw Data:<br />";
+    //return $data;
+    $data_tab = str_split($data,33);
+    echo "Tabular Data:<br />";
+    return $data_tab;
+}
 
+function trame($bdd, $trame){
+    // décodage avec des substring
+    $t = substr($trame,0,1);
+    $o = substr($trame,1,4);
+    $r = substr($trame,5,1);
+    $c = substr($trame,6,1);
+    $n = substr($trame,7,2);
+    $v = substr($trame,9,4);
+    $a = substr($trame,13,4);
+    $x = substr($trame,17,2);
+    $year = substr($trame,19,4);
+    $month = substr($trame,23,2);
+    $day = substr($trame,25,2);
+    $hour = substr($trame,27,2);
+    $min = substr($trame,29,2);
+    $sec = substr($trame,31,2);
+    echo("<br />$t,$o,$r,$c,$n,$v,$a,$x,$year,$month,$day,$hour,$min,$sec<br />");
+
+    if($c == 3){
+        $id = $n;
+        $date = $year + "-" + $month + "-" + $day + " " + $hour + ":" + $min + ":" + $sec;
+        $donnee = $v * 0.001;
+
+    }
+    if(empty($donnee)){
+        echo "non";
+    }
+
+    $req = $bdd->prepare("INSERT INTO donnees(donnee, id_capteur, date) VALUES (:donnee, :idcapteur, :date)");
+    $req->bindParam(':donnee', $donnee);
+    $req->bindParam(':idcapteur', $id);
+    $req->bindParam(':date', $date);
+    $req->execute();
+    echo "Vous avez réussi !";
+}
 ?>
